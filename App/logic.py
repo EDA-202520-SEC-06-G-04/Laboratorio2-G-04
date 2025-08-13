89b334874c40568d8fedb3140029a9f4011cbef9
JUANPABLO2007\penar <penarandajuan868@gmail.com>
2025-08-13 15:23:02 -0500

    actualizacion Est 3

--- App/logic.py
+++ App/logic.py
@@ -133,7 +133,16 @@ def load_books_tags(catalog, filename):
     :rtype: int
     """
     # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    pass
+    """
+Cargo los tags de los libros del archivo
+"""
+    # TODO: Mods de Est-1 y Est-2, Est-3 en el Lab 2
+    booktagfile = os.path.join(data_dir, filename)
+    input_file = csv.DictReader(open(booktagfile, encoding="utf-8"))
+    catalog = create_book_tag_list(catalog)
+    for booktag in input_file:
+        add_book_tag(catalog, booktag)
+    return book_tag_size(catalog)
 
 
 def first_book(catalog):
@@ -141,7 +150,7 @@ def first_book(catalog):
     Devuelve el primer libro del catalogo
     """
     # TODO: Mods Est-3 en el Lab 2
-    pass
+    return set.get_first_element(catalog["books"])
 
 
 def last_book(catalog):
@@ -149,8 +158,7 @@ def last_book(catalog):
     Devuelve el ultimo libro del catalogo
     """
     # TODO: Mods Est-3 en el Lab 2
-    pass
-
+    return set.get_last_element(catalog["books"])
 # Funciones para la manipulacion de los datos
 
 
@@ -167,7 +175,8 @@ def add_book_tags_file(catalog, booktagsfile):
     :rtype: dict
     """
     # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    pass
+    set.add_element(catalog["book_tags"], booktag)
+    return catalog
 
 
 def create_book_tag_list(catalog):
